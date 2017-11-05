6f944f5a3c73c202eaa62d7fbb14619b56558a4d
Drew Ripberger <drew.ripberger@gmail.com>
2017-11-05 03:10:35 -0500

    Update inventio.sol

--- inventio.sol
+++ inventio.sol
@@ -1,11 +1,64 @@
 pragma solidity ^0.4.0;
 contract invenio{
     
-    struct user{
-        uint16 papers;
-        bool reviewed;
-        string username;
-        address delegate;
+    struct User{
+        string[] papers;
+        string[] reviewed ;
+        string firstName;
+        string lastName;
+        address userId;
+    }
+    
+    User[] private allUsers;
+    
+    User private currentUser;
+    function checkOwner(address checkAddr) returns(bool){
+        if(checkAddr == msg.sender){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    function getUser(address getAddr) private returns(bool){
+        for(uint64 i = 0; i <= allUsers.length; i++){
+            if(allUsers[i].userId == getAddr){
+                currentUser = allUsers[i];
+            }
+        }
+    }
+    
+    function checkColl(address checkAddr) private returns(bool){
+        for(uint64 i = 0; i <= allUsers.length; i++){
+            if(allUsers[i].userId == checkAddr){
+                return true;
+            }
+            return false;
+        }
     }
 
+    function newUser(string newFirstName, string newLastName){
+        if(!checkColl(msg.sender)){
+            User newUser;
+            newUser.firstName = newFirstName;
+            newUser.lastName = newLastName;
+            newUser.userId = msg.sender;
+            allUsers.push(newUser);
+        }
+        else{
+            require(false);
+            return;
+        }
+    }
+    function uploadPaper(address toAddress) public{
+        require(checkOwner(toAddress));
+        
+    }
+    event print(address out);
+    function checkAccounts() public{
+        for(uint64 i = 0; i <= allUsers.length; i++){
+            print(allUsers[i].userId);
+        }
+    }
 }
